1566ce5b4a95bd2ab5922249d7897641fa237b47
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-04 21:17:13 +0530

    added files

---
+++ sum_of_numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	//Take Input
+	cin>>n;
+	//Init Condition
+	int i=1;
+	int sum = 0;
+
+	while(i<=n) {
+		sum = sum+i;
+		i = i+1;
+	}
+	cout<<sum<<endl;
+
+	return 0;
+}

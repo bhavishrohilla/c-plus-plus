aa2bea49c791a3b0d900175cfc2aee1d077623c7
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-04 17:26:20 +0530

    updated files

--- simple_interest.cpp
+++ simple_interest.cpp
@@ -8,13 +8,18 @@ int main() {
 	int p,r,t;
 	float si;
 
-	//assigning value to the storage in the memory
+	/*assigning value to the storage in the memory
 	p=50;
 	r=25;
 	t=1;
+	*/
+
+	//taking input from the user
+	cin>>p;
+	cin>>r>>t;
 
 	//every statement should be teminated by the semi-colon
-	si = p*r*t/100;
+	si = p*r*t/100.0;
 	cout<< si <<endl;
 
 

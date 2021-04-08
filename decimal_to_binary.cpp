2d5bcf17349ed531f55d61dca99932641f0c9450
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-08 14:37:01 +0530

    updated decimal to binary

--- decimal_to_binary.cpp
+++ decimal_to_binary.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main() {
 	int no;
 	int N; 
-	cin>>N;
+	cin>>N; //the no. of binary digita input
 
 	while(N>0){
-		cin>>no;
-		int ans=0;
-		int p=1;
+		cin>>no;  //the binary digits input
+		int ans=0;  //the sum of the digits should be 0 initially
+		int p=1;  // raise to the power  means 2 raise to the power 0 = 1
 		while(no>0){
 			int r=no%10;
 			ans = ans+r*p;
-			p = p*2;
-			no = no/10;
+			p = p*2;   // the power is inceremented by 2 raise to the power like 1,2,4,8,16,32
+			no = no/10; // to discard the last digit like 101/10= 10 now the work is further done on 0
 		}
 	cout<<ans<<endl;
 	N=N-1;

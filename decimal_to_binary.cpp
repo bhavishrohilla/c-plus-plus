ee946985f306232c0999f469ece2d0691d139849
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-08 14:35:01 +0530

    updated decimal to binary

--- decimal_to_binary.cpp
+++ decimal_to_binary.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin>>n;
+	int no;
+	int N; 
+	cin>>N;
 
-	int ans=0;
-	int p=1;
-	while(n>0){
-		int r=n%10;
-		ans = ans+r*p;
-		p = p*2;
-		n = n/10;
-	}
+	while(N>0){
+		cin>>no;
+		int ans=0;
+		int p=1;
+		while(no>0){
+			int r=no%10;
+			ans = ans+r*p;
+			p = p*2;
+			no = no/10;
+		}
 	cout<<ans<<endl;
+	N=N-1;
+	}
 
 
 

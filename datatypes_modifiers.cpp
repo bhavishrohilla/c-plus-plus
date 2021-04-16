cf7ca565a78a3e031a4a0a19831992305165f85f
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-16 15:38:16 +0530

    datatypes modifiers

--- datatypes_modifiers.cpp
+++ datatypes_modifiers.cpp
@@ -10,9 +10,12 @@ int main(){
 	int x;
 
 	cout<<sizeof(x)<<endl;
-	cout<<CHAR_MAX<<endl;
-	cout<<CHAR_MIN<<endl;
 
+	x = INT_MAX;
+	cout<<"X : "<<x<<endl;
+
+	x = x+10; //As we are adding 1 to the max value of int, it will be added to the min value of int
+	cout<<"Updated X : "<<x<<endl;
 
 	return 0;
 }

eb91d2ef634809a422f443c15376c7ffed404159
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-22 16:38:05 +0530

    scope of a variable

---
+++ variable_scope.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+//Scope is defined for variables
+//defines how accessible a variablle is.(lifetime or visibility)
+//locak scope and global scope
+int main(){
+
+
+
+
+	return 0;
+}

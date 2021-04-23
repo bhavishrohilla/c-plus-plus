b0a5c46421371679b1c1049f4eb58c057f21b93a
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-23 13:17:38 +0530

    variable scope

--- variable_scope.cpp
+++ variable_scope.cpp
@@ -5,9 +5,12 @@ using namespace std;
 //Scope is defined for variables
 //defines how accessible a variablle is.(lifetime or visibility)
 //locak scope and global scope
+
+int x = 100;
 int main(){
-
-
+	int x = 10;
+	cout<<x<<endl;
+	cout<<::x;		//Scope resolution operator gives the global variable
 
 
 	return 0;

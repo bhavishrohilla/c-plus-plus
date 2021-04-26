87b445413df4093a1a777fecb8b3d89ba87d4585
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-26 18:01:34 +0530

    updated reduced string

--- reduced_string.cpp
+++ reduced_string.cpp
@@ -26,5 +26,25 @@ int main(){
 		}
 		ch = cin.get();
 	}
-	cout<<"The final displacement is "<<x <<" and "<<y;
+	if(x>0){
+		for(int i=0;i<=x;i++){
+			cout<<'E';
+		}
+	}
+	else if(x<0){
+		for(int i=0;i<=x;i++){
+			cout<<'W';
+		}
+	}
+	if(y>0){
+		for(int j=0;j<=y;j++){
+			cout<<'N';
+		}
+	}
+	else if(y<0){
+		for(int j=0;j<=y;j++){
+			cout<<'S';
+	}
+	}
+	
 }

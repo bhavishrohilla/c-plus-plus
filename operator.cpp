8d64178a82057f568daa5fc4bbdd4e78c2017a35
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-04-30 11:34:03 +0530

    operators

--- operator.cpp
+++ operator.cpp
@@ -7,4 +7,11 @@ int main(){
 	int y = x++;  //increment is after the assigment so x will be incremented(100) whereas y will not be incremented(100).
 	cout<< x <<endl;
 	cout << y;
+
+
+	int a =100;
+	int b = ++a;	//
+	cout<<a<<endl;
+	cout<<b;
 }
+

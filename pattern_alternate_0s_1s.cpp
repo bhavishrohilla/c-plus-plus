edf72647cff86ada10951c9daeba1251ddc12507
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-05 15:54:29 +0530

    pattern containing 0 and 1

---
+++ pattern_alternate_0s_1s.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+/*
+-for ith line print i numbers
+-odd line starts with 1 and value alters between 0 and 1
+-even line starts with 0 and value alters between 0 and 1
+-print n lines
+n = 5
+0
+10
+010
+1010
+01010
+*/
+int main(){
+	int n ;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		int val = i%2==0?1:0;
+		for(int count=1;count<=i;count++){
+			cout<<val;
+			val = 1-val; 
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
--- switch_case.cpp
+++ switch_case.cpp
@@ -4,7 +4,21 @@ using namespace std;
 int main(){
 	//Switch case 
 	//Design a menu where pressing a button gives  you the corresponding item
+	char ch;
+	cin>>ch;
 
+	switch(ch){
+		case 'b':
+		case 'B' : cout<<"Burger"<<endl;
+			break;
+		case 'm':
+		case 'M' : cout<<"Maggi"<<endl;
+			break;
+		case 
+		case 'P' : cout<<"Pizza"<<endl;
+			break;
+		default  : cout<<"Item Not Available! Please make at your own.";
+	}
 
 
 }

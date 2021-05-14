24157a376b3eaf97d84b4d1e715f0228508c2863
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-14 11:09:40 +0530

    arrays

--- Functions/array.cpp
+++ Functions/array.cpp
@@ -7,9 +7,12 @@ int main(){
 	int b[10] = {5}; //5,0,0,......,0
 	int c[10] = {1,2,3}; //1,2,3,0,0,0.....,0
 	int n;
-	cin>>n;
+	/*cin>>n;
 	int d[n];
 	for(int i=0; i<n; i++){
 		cin>>d[i];
+	}*/
+	for(int i=0; i<10; i++){
+		cout<<c[i]<<", ";
 	}
 }

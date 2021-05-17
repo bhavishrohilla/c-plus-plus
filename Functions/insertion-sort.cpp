d27e048eb3216c4f8107bc41ab98defc43ad6237
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-17 14:13:23 +0530

    insertion sort

--- Functions/insertion-sort.cpp
+++ Functions/insertion-sort.cpp
@@ -1,14 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+	for(int i=1; i<=n-1; i++){
+		int e = arr[i];
+		int j= i-1;
+		while(j>=0 and arr[j]>e){
+			arr[j+1]=arr[j];
+			j = j - 1;
+		}
+		arr[j+1] = e;
+	}
+}
+
 int main(){
 	int n;
 	cout<<"Size of Array- ";
 	cin>>n;
 	cout<<"Enter "<<n<<" elements "<<endl;
-	int a[n];
+	int arr[n];
 	for(int i=0; i<n; i++){
-		cin>>a[i];
+		cin>>arr[i];
 	}
-	bubbleSort(a,n);
+	selectionSort(arr,n);
 	for(int x=0; x<n; x++){
-		cout<<a[x]<<", ";
+		cout<<arr[x]<<", ";
 	}
 }

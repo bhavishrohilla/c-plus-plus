6de6b9a63f410529d1b7ca2788b9efb35caa5c04
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-18 18:47:20 +0530

    compare function

--- Functions/inbuilt-sort.cpp
+++ Functions/inbuilt-sort.cpp
@@ -2,6 +2,11 @@
 #include<algorithm> //includes inbuilt sorting functions already
 using namespace std;
 
+//Defining a comparator
+bool compare(int a, int b){
+	return a>b;
+}
+
 int main(){
 
 	int n;
@@ -11,6 +16,11 @@ int main(){
 		cin>>a[x];
 	}
 	sort(a,a+n+1);
+	for(int x=0; x<n; x++){
+		cout<<a[x];
+	}
+
+	sort(a,a+n+1, compare); //compare changes it to Decreasing order
 	for(int x=0; x<n; x++){
 		cout<<a[x];
 	}

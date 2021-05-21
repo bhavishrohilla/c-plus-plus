4f0d325c107d3d71b5db98e2b28255c87d599586
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-21 21:05:50 +0530

    wave print of 2D array

--- questions/wave_print.cpp
+++ questions/wave_print.cpp
@@ -14,6 +14,21 @@ int main(){
 		}
 		cout<<endl;
 	}
+	//Wave Print
+	for(int col=0; col<=n-1; col++){
+		if(col%2==0){
+			//Even column - top to bottom direction
+			for(int row=0; row<=m-1; row++){
+				cout<<a[row][col]<<" ";
+			}
+		}
+		else{
+			//Odd column - bottom up direction
+			for(int row=m-1; row>=0; row--){
+				cout<<a[row][col]<<" ";
+			}
+		}
+	}
 
 
 	return 0;

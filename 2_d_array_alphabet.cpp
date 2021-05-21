3f00eba9487fd5d4ce0b4aeb2ca015d96627b878
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-21 15:02:26 +0530

    2d array aplhabet size

--- 2_d_array_alphabet.cpp
+++ 2_d_array_alphabet.cpp
@@ -5,12 +5,13 @@ int main(){
 	int m,n;
 	cin>>m>>n;
 	char a[m][n];
+	cout<<"Size of this 2D array is - "<<sizeof(a)<<endl;
 	char val = 'A';
 	for(int row=0;row<=m-1; row++){	//As indexing starts from 0, the index of last row(m th) will be m-1.
-		for(int col=1; col<n-1; col++){
+		for(int col=0; col<=n-1; col++){
 			a[row][col] = val;
 			val +=1;
-			cout<<a[row][col];
+			cout<<a[row][col]<<" ";
 		}
 		cout<<endl;
 	}

d8637cd38484f0b4fb4ce8e308b4ef3ffdb0745b
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-22 17:22:25 +0530

    spiral print

---
+++ questions/print_spiral.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int m,n;
+	cin>>m>>n;
+	int a[m][n];
+	int val = 1;
+	for(int row=0; row<=m-1; row++){
+		for(int col=0; col<=n-1; col++){
+			a[row][col] = val;
+			val+=1;
+			cout<<a[row][col]<<" ";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}

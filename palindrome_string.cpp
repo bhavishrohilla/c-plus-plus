93a26359a3e95cfde227883b0fce7faca4279b69
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-28 12:45:42 +0530

    palindrome string

---
+++ palindrome_string.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+bool isPalindrome(int a[]){
+	int i=0;
+	int j=strlen(a) -1;
+	while(i<j){
+		if(a[i]==a[j]){
+			i++;
+			j--;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(){
+
+	int char a[1000];
+	cin.getline(a, 1000);
+	if(isPalindrome(a)){
+		cout<<"Palindrome"<<endl;
+	}
+	else{
+		cout<<"Not a Palindrome "<<endl;
+	}
+
+	return 0;
+}

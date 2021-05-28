4c44c654e3a14d4bc74c06584986ce4f27d2e1e0
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-28 23:21:20 +0530

    sorting

---
+++ Strings/sorting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	cin.get();
+	string s[100]; //Vector
+	for(int i=0; i<n; i++){
+		getline(cin, s[i]);
+	}
+
+
+
+	return 0;
+}

de298d7b1ac6b8362740b586736dd6064074a86e
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-28 13:21:55 +0530

    remove duplets

--- skip_repetetion.cpp
+++ skip_repetetion.cpp
@@ -3,9 +3,23 @@
 using namespace std;
 
 void removeDuplets(char a[]){
-
+	int l = strlen(a);
+	if(l==0 or l==1){
+		return;
+	}
+	int prev = 0;
+	for(int curr=1; curr<l; curr++){
+		if(a[curr]==a[prev]){
+			a[prev] = a[curr];
+			prev++;
+		}
+	}
+	a[prev + 1] = '\0';
+	return;
 }
 int main(){
 	char a[100];
 	cin.getline(a,100);
+	removeDuplets(a);
+	cout << a << endl;
 }

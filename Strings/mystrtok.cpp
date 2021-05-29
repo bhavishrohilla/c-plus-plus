aea9fe86e3416da8cfdb5078e8b8ee8fbc56546c
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-05-29 23:14:32 +0530

    my tokenization

--- Strings/mystrtok.cpp
+++ Strings/mystrtok.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<cstring>
+#include<algorithm>
 using namespace std;
 
 char *mystrtok(char *str, char delim){
@@ -6,15 +8,33 @@ char *mystrtok(char *str, char delim){
 	//string and single char as delimiter
 	static char *input = NULL;
 	if(str!= NULL){
+		//making the first call
 		input = str;
 	}
+	//check here - base case after the finsl token has been returned
+	if(input = NULL){
+		return NULL;
+	}
+
 
 	//start xtracting tokens and store them in an array
 	char *output = new char[strlen(input)+1];
 	int i=0;
 	for( ;input [i]!='\0';i++){
-		
+		if(input[i]!=delim){
+			output[i] = input[i];
+		}
+		else{
+			output[i] = '\0';
+			input = input + 1;
+			return output;
+		}
 	}
+	//corner case
+	output[i] = '\0';
+	input = NULL;
+	return output;
+
 }
 
 int main(){
@@ -24,7 +44,7 @@ int main(){
 	cout<<ptr<<endl;
 
 	while(ptr!=NULL){
-		ptr = mystrtok(NULL, " ");
+		ptr = mystrtok(NULL, ' ');
 		cout<<ptr<<endl;
 	}
 

8e00c49e363d93133e20da727d5941485f86cab7
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-06 12:59:18 +0530

    pointers

--- pointers/address.cpp
+++ pointers/address.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
 	int x = 10;
-	cout<< &x << endl;
+	//cout<< &x << endl;
 
 	float y = 	10.7;
 
@@ -19,6 +19,10 @@ int main(){
 	//Explicit type cadting from char* to void*
 	cout<< (void *)&ch << endl; // void * will tell the address of the character
 
+	int *ptr;
+	ptr = &x;
+	cout << ptr << endl;
+	cout << x << endl;
 
 	return 0;
 }

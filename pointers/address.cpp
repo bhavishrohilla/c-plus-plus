25154e996c8a86412427e805be421806aa5efbfa
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-06 13:03:06 +0530

    reassigning another variable

--- pointers/address.cpp
+++ pointers/address.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 
 	int x = 10;
+	int y1 = 20;
 	//cout<< &x << endl;
 
 	float y = 	10.7;
@@ -22,7 +23,12 @@ int main(){
 	int *ptr;
 	ptr = &x;
 	cout << ptr << endl;
-	cout << x << endl;
+	cout << &x << endl;
 
+
+	//reassign another address to the variable
+	ptr = &y1;
+	cout << &y1 << endl;
+	cout << ptr << endl;
 	return 0;
 }

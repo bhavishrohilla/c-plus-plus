8d5cfd0be6a6277c1cf6b06f5df8d7bfdcf3614e
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-09 12:42:44 +0530

    pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -7,6 +7,7 @@ int main()
 	int *xptr = &x;
 	cout<< xptr << endl;	//address of the the storage where the x is stored.
 	cout<< *(&x) << endl;	//x that is 10.
+	cout<< *(&x) + 1 << endl;		//11
 
 	return 0;
 }

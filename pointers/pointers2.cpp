e1ade023834cceca22ff143b92522d11165b49ee
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-09 13:18:10 +0530

    double pointer

--- pointers/pointers2.cpp
+++ pointers/pointers2.cpp
@@ -12,11 +12,19 @@ int main(){
 		cout << xptr << endl;		//address of x
 		cout << &x << endl;		//address of x
 		cout << endl;
+
 		cout << *(&x) << endl;		//10
 		cout << *(xptr) << endl; 		//10
 		cout << endl;
-		cout << *(&xptr) << endl;		//
-		cout << &(*xptr) << endl;
+
+		cout << *(&xptr) << endl;		//address of x
+		cout << &(*xptr) << endl;		//address of x
+
+		cout << &xptr << endl;
+
+		//Double pointer
+		int **xxptr = &xptr;
+		cout << xxptr << endl;
 
 
 	return 0;

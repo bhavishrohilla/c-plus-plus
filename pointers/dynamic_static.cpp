a170f0f91b85f3d68c11d48aa5f21fff7c06871b
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-11 10:30:54 +0530

    dynamic_static

--- pointers/dynamic_static.cpp
+++ pointers/dynamic_static.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
 
 	//Allocation
-	int b[100];
+	int b[100] = {0};
 	cout << sizeof(b) << endl;
 
 	//Dynamic Allocation (on the fly)
 	int n;
 	cin >> n;
-	int *a = new int[n];
+	int *a = new int[n]{0};
 	cout << sizeof(a) << endl;
 
 	//NO Change

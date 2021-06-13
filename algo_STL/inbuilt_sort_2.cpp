267440504489acf79010612a1110282ed066ba1f
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-13 19:05:04 +0530

    inbuilt sort decreasing function

--- algo_STL/inbuilt_sort_2.cpp
+++ algo_STL/inbuilt_sort_2.cpp
@@ -2,8 +2,14 @@
 #include<algorithm>
 using namespace std;
 
+//Declare a comparator
+bool compare(int a, int b){
+	return a > b;
+}
+
 int main(){
-	//In increasing order
+
+	//In decreasing order
 	 int n,key;
 	 cout << "No. of elements you have to input - "; 
 	 cin >> n;
@@ -12,7 +18,7 @@ int main(){
 	 	cin >> a[i];
 	 }
 	 //Sort an array using sort function
-	 sort(a, a+n);
+	 sort(a, a+n, compare);
 
 	 for(int i=0; i<n; i++){
 	 	cout << a[i] << " ";

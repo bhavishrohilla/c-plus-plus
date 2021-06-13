fdf97b0928739415dffb4d9f7c4ea9b3eebf4181
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-13 17:48:47 +0530

    2d_array_dynamic

---
+++ pointers/2d_dynamic_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int **arr;
+	int r, c;
+	cin >> r >> c;
+
+	//Create an array of row heads
+	arr = new int*[r];
+	
+
+	return 0;
+}
--- pointers/dynamic_static.cpp
+++ pointers/dynamic_static.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int main(){
 
-	//Allocation
+	//Allocation + deallocation is done by compiler
 	int b[100] = {0};
 	cout << sizeof(b) << endl;
+	cout << b << endl; //Symbol table
+	//here b can be overwritten, b is a constant
 
 	//Dynamic Allocation (on the fly)
 	int n;
 	cin >> n;
 	int *a = new int[n]{0};
 	cout << sizeof(a) << endl;
+	cout << a << endl; //variable a that is created inside the staitc memory -> overwritten
+	
+
 
 	//NO Change
 	for(int i=0; i<n; i++){

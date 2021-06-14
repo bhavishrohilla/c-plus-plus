52f708aa89575d654dcafc48d2d323491efb172c
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-14 10:38:03 +0530

    rotate function vectors

--- algo_STL/rotate.cpp
+++ algo_STL/rotate.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -9,5 +10,13 @@ int main(){
 
 	for(int i=0; i<n; i++){
 		cout << arr[i] << " ";
+	}
+	cout << endl;
+	//Apply the same on vector
+	vector<int> v{10,20,30,40,50};
+	rotate(v.begin(), v.begin()+3,v.end());
+
+	for(int i=0; i<v.size(); i++){
+		cout << v[i] << " ";
 	}
 }

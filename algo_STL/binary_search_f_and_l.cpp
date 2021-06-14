fe4bbe4fa9c8782687514f9eb476dadeed2e5b35
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-14 14:14:09 +0530

    first occurence

--- algo_STL/binary_search_f_and_l.cpp
+++ algo_STL/binary_search_f_and_l.cpp
@@ -2,14 +2,28 @@
 #include<algorithm>
 using namespace std;
 
-int first_occurence(int a, int b, key){
+int first_occurence(int a[], int n, int key){
 	int s = 0;
 	int e = n-1;
 	int ans = -1;
 
-	while
-}
+	while(s<=e){
+		int mid = (s+e)/2;
 
+		if(a[mid]==key){
+			//Do two Things
+			ans = mid;
+			e = mid - 1;
+		}
+		else if(a[mid]>key){
+			e = mid - 1;
+		}
+		else{
+			s = mid + 1;
+		}
+	}
+	return ans;
+}
 int main(){
 	int arr[] = {1,2,5,8,8,8,8,10,12,15,20};
 	int n = sizeof(arr)/sizeof(int);
@@ -17,7 +31,7 @@ int main(){
 	cin >> key;
 
 	//First and Last Occurence of the key in sorted array
-
+	cout << first_occurence(arr,n,key);
 
 	return 0;
 }

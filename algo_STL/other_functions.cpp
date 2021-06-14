6c769a82be57d6f8d9cd96aa7cd567bc62ac34b4
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-14 12:33:38 +0530

    other functions

--- algo_STL/other_functions.cpp
+++ algo_STL/other_functions.cpp
@@ -13,4 +13,11 @@ int main(){
 
 	cout << min(a,b) << endl;
 	cout << max(a,b) << endl;
+
+	int arr[] = {1,2,3,4,5,6};
+	reverse(arr, arr+4);
+	int n = sizeof(arr)/sizeof(int);
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<" ";
+	}
 }

a12f6e1a6cc751dc31879dc31f82efb99bfdf38c
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-14 13:17:27 +0530

    pair class

--- algo_STL/pair_class.cpp
+++ algo_STL/pair_class.cpp
@@ -17,5 +17,21 @@ int main(){
 
 	pair<int,string> p3 = make_pair(100,"Audi");
 	cout<< p3.first << " " << p3.second << endl;
+	
+	//Take input
+	int x,y;
+	cin >> x >> y;
+	pair<int,int> p4 = make_pair(x,y);
+	cout << p4.first <<" "<<p4.second;
+	cout << endl;
+	//Array of pairs
+	//Vector of pairs
+	pair<pair<int,int>,string> car;
+	car.first.first = x;
+	car.second = "Mercedes";
+	car.first.second = y;
+	cout << car.second << " went from "<<car.first.first << " to " << car.first.second;
+
+
 	return 0;
 }

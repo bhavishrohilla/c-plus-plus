af2a52956586fbc86489d1130224d8ef3845ecb7
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-15 23:07:06 +0530

    builtin_popcount

--- bitmasking/set_bit_count.cpp
+++ bitmasking/set_bit_count.cpp
@@ -18,12 +18,17 @@ int countBitsFaster(int n){
 	while(n > 0){
 		n = n & (n-1);	//It will remove the set bits from right to left(time complexity is low)
 		ans++;
-	}	//no. of iterations will be equal to the no. of times loop executes 
+	}	//no. of iterations will be equal to the no. of times loop executes
+	return ans; 
 }
+
+//Third method is builtin popcount
 
 int main(){
 	int n;
 	cin >> n;
 	cout << countBits(n) << endl;
 	cout << countBitsFaster(n) << endl;
+	//3
+	cout << __builtin_popcount(n) << endl;
 }

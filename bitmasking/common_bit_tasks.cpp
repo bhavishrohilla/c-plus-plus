2f1c6f792d0821175a9570ca270ce32b57ec9162
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-15 12:34:41 +0530

    bitmasking

---
+++ bitmasking/common_bit_tasks.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+bool isOdd(int a){
+	return (a & 1); //if it is 1 then the no. is odd
+}
+
+int main(){
+
+	int n = 5;
+	int i = 2;
+	cout << isOdd(n);
+
+}

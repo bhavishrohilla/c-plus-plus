8ce3146e5dc4e63e9e2912a33db271ed782429a2
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-15 17:32:05 +0530

    get bit

--- bitmasking/common_bit_tasks.cpp
+++ bitmasking/common_bit_tasks.cpp
@@ -5,9 +5,20 @@ bool isOdd(int a){
 	return (a & 1); //if it is 1 then the no. is odd
 }
 
+int getBit(int n, int i){
+	int mask = (1 << i);
+	int bit = (n & mask) > 0 ? 1 : 0;
+	return bit;
+}
+
 int main(){
 
 	int n = 5;
-	int i = 2;
-	cout << isOdd(n);
+	cout << "Which bit you want to know ? - ";
+	int i;
+	cin >> i;
+	cout <<"The "<<i<<" bit is "<< getBit(n,i)<<endl;
+	//cout << isOdd(n);
+
+	return 0;
 }

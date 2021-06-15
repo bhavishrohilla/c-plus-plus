a9a494d5c6fcf77a0c0d3b8c014431214285d910
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-06-15 18:49:07 +0530

    clearing range of bits

--- bitmasking/clearing_range_bits.cpp
+++ bitmasking/clearing_range_bits.cpp
@@ -6,12 +6,25 @@ int clearLastBits(int n, int i){
 	n = (n & mask);
 	return n ;
 }
+int clearRangeFromItoJ(int n, int i, int j){
+	int a = (~0) << (j+1);
+	int b = (1 << i) -1;
+	int mask = a | b;
+	int ans = n & mask;
+	return ans;
+}
 
 int main(){
 
-	int n = 5;
-	int i;
+	int n = 31;
+	int i = 1;
+	int j = 3;
+	/*
+	cout << "How many bits you want to delete or make 0 from right ofcourse ? - ";
 	cin >> i;
-	cout << clearLastBits(n,i)<< endl;
+	cout << "Ans " <<clearLastBits(n,i)<< endl;
+	*/
+	cout << clearRangeFromItoJ(n, i, j);
 
+	return 0;
 }

1d1a95a01db4399ba22b62d72fe607973a70303e
unknown <bhavishrohilla59@gmail.com>
2021-06-18 02:22:34 -0700

    unique no

--- questions/unique number/unique_no_3.cpp
+++ questions/unique number/unique_no_3.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main(){
 	int cnt[64] = {0};
-	int n;
+	int n,no;
 	cin>>n;
 	for(int i=0; i<n;i++){
-		
+		cin>>no;
 	}
+	//UPdate cnt array w=by extracting bits
+	int j=0;
+	while(no>0){
+		int last_bit = (no&1);
+		cnt[j] += last_bit;
+		j++;
+		no = no>>1;
+	}
+
 }

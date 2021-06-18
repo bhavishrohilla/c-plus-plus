c3da1373bf3c28b5cb7448f066ac5615ffab7e10
unknown <bhavishrohilla59@gmail.com>
2021-06-18 02:28:17 -0700

    unique no 2

--- questions/unique number/unique_no_3.cpp
+++ questions/unique number/unique_no_3.cpp
@@ -16,5 +16,16 @@ int main(){
 			no = no>>1;
 		}
 	}
+	//iterate over the array and form the ans by converting 0s and 1s into number
+	int p=1;
+	int ans =0;
+	for(int i=0; i<64; i++){
+		cnt[i] %= 3;
+		ans += (cnt[i]*p);
+		p = p << 1;
+	}
+
+	//Print the final ans
+	cout << ans<<endl;
 
 }

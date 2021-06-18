c73a59c4363b088dbf6cd610a8b5eda0d12b3320
unknown <bhavishrohilla59@gmail.com>
2021-06-18 02:45:37 -0700

    power fast

--- questions/unique number/power_fast.cpp
+++ questions/unique number/power_fast.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int power_optimised(int a, int n){
+	int ans = 1;
+	while(n>0){
+		int last_bit = (n&1);
+		if(last_bit){
+			ans = ans *a; 
+		}
+		a = a*a;
+		n = n>>1;
+	}
+	return ans;
+}
+
 int main(){
+	/*
 	int dig, pow;
 	cin >> dig>>pow;
 	int ans=1;
@@ -9,4 +23,9 @@ int main(){
 		ans = ans * dig;
 	}
 	cout << ans;
+	*/
+	int n,a;
+	cin >> a >> n ;
+	cout << power_optimised(a,n) <<endl;
+
 }

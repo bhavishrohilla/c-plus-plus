c2268c6e768297d6029a25ab9fdf32080ca241c5
unknown <bhavishrohilla59@gmail.com>
2021-06-19 13:44:57 +0530

    prime factorization optimised

--- questions/unique number/PF_optimised_trial.cpp
+++ questions/unique number/PF_optimised_trial.cpp
@@ -11,9 +11,20 @@ void printFactors(int n){
 			cnt = 0;
 			while(n%i==0){
 				cnt++;
+				n = n / i;
 			}
+			factors.push_back(make_pair(i,cnt));
 		}
 	}
+	if(n!=1){
+		//whatever is left is a prime no itself
+		factors.push_back(make_pair(n,1));
+	}
+	//print the factors
+	for(auto p:factors){
+		cout << p.first<<" "<<p.second<<endl;
+	}
+	return;
 }
 
 int main(){

85e14fb3453e5876bd3a49d9643762d3acc4fd76
unknown <bhavishrohilla59@gmail.com>
2021-06-19 13:41:15 +0530

    prime factorization using seive

--- questions/unique number/PF_optimised_trial.cpp
+++ questions/unique number/PF_optimised_trial.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 void printFactors(int n){
+	vector<pair<int, int> > factors;
+	int cnt;
 	for(int i=2; i*i<=n;i++){
 		if(n%i==0){
 			//keep on dividing it by i till it is divisible
+			cnt = 0;
 			while(n%i==0){
-				
+				cnt++;
 			}
 		}
 	}

771b8df7302baadb2ae297967249b8d148ebbc85
unknown <bhavishrohilla59@gmail.com>
2021-06-19 09:41:47 +0530

    prime factrization using seice

--- questions/unique number/prime_factorization_using_seive.cpp
+++ questions/unique number/prime_factorization_using_seive.cpp
@@ -2,6 +2,22 @@
 #include<vector>
 using namespace std;
 
+vector<int> primeSeive(int *p, int n){
+	p[0] = p[1] = 0;
+	p[2] = 1;
+
+	//Let us mark all odd numbers as prime
+	for(int i=3; i<=n; i+=2){
+		p[i] = 1;
+	}
+
+	//Seive login to mark non prime numbers as zero
+	//1. Optimization - Iterate only over odd numbers
+	for(int i=3; i<=n; i+=2){
+		if(p[i])
+	}
+}
+
 int main(){
 	/* code */
 	int N = 1000000;

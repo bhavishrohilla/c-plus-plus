a477b21bfaaffd087abf54b87220723dd27355d1
unknown <bhavishrohilla59@gmail.com>
2021-06-20 11:03:03 +0530

    includion exclusion

---
+++ questions/unique number/includion_exclusion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+
+int main(){
+	ll t;
+	cin >> t;
+	ll primes[] = {2,3,5,7,11,19};
+	while(t--){
+		ll n;
+		cin >> n;
+
+		ll subsets = (1<<8);
+	}
+
+
+
+	return 0;
+}

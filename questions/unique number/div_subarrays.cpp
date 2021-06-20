69eee75eacc813100bddfb2efb298c5b4bf2fecf
unknown <bhavishrohilla59@gmail.com>
2021-06-20 08:55:34 +0530

    div subarrays

--- questions/unique number/div_subarrays.cpp
+++ questions/unique number/div_subarrays.cpp
@@ -13,7 +13,20 @@ int main(){
 		memset(pre,0, sizeof(pre));
 		pre[0] = 1;
 		//read the input
-		
+		int sum = 0;
+		for(int i=0; i<n; i++){
+			cin >> a[i];
+			sum += a[i];
+			sum %= n;
+			sum= (sum+n) % n;
+			pre[sum]++;
+		}
+		ll ans = 0;
+		for(int i=0; i<n; i++){
+			int m = pre[i];
+			ans += m*(m-1)/2;
+		}
+		cout << ans << endl;
 	}
 
 

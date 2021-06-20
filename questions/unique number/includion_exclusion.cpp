a3e6c0e2b2f1725e196dc1022d6e6afbac77e309
unknown <bhavishrohilla59@gmail.com>
2021-06-20 11:10:14 +0530

    inclusion exclusion

--- questions/unique number/includion_exclusion.cpp
+++ questions/unique number/includion_exclusion.cpp
@@ -12,9 +12,28 @@ int main(){
 		cin >> n;
 
 		ll subsets = (1<<8)-1;
+		ll ans = 0;
 		for(ll i=1; i<=subsets; i++){
 			ll denom = 1ll;
 			ll setBits = __builtin_popcount(i);
+			for(ll j=0; j<=7; j++){
+				if(i & (1<<j)){
+					denom = denom*primes[j];
+
+				}
+			}
+
+			if(setBits&1){
+				ans += n/denom;
+
+			}
+			else{
+				ans -= n/denom;
+
+
+
+
+			}
 
 
 		}

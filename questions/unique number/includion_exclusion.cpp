cf6a99a75e46aeec90e31285d65f3e5a78444914
unknown <bhavishrohilla59@gmail.com>
2021-06-20 11:06:39 +0530

    inclusion exclusion

--- questions/unique number/includion_exclusion.cpp
+++ questions/unique number/includion_exclusion.cpp
@@ -11,7 +11,16 @@ int main(){
 		ll n;
 		cin >> n;
 
-		ll subsets = (1<<8);
+		ll subsets = (1<<8)-1;
+		for(ll i=1; i<=subsets; i++){
+			ll denom = 1ll;
+			ll setBits = __builtin_popcount(i);
+
+
+		}
+
+
+
 	}
 
 

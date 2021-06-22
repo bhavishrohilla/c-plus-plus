b15da88232f41bb58580a1c3cd455db7e3e664f0
unknown <bhavishrohilla59@gmail.com>
2021-06-22 18:47:50 +0530

    binary_search recursion

--- recursion/binary_search.cpp
+++ recursion/binary_search.cpp
@@ -1,31 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int binarySearch(int a[], int key, int n){
-	int s = 0;
-	int e = n-1;
-	while(s<=e){
-		int mid = (s+e)/2;	
-		if(a[mid] == key){
-			return mid;
-		}
-		else if(a[mid]>key){
-			e = mid -1;
-		}
-		else{
-			s = mid + 1;
-		}
-	}
-	
+int binarySearch(int arr[], int l, int r, int key){
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+  
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == key){
+            return mid;
+        }
+        
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > key){
+            return binarySearch(arr, l, mid-1, key);
+        }
+  
+        // Else the element can only be present
+        // in right subarray
+        return binarySearch(arr, mid+1, r, key);
+    }
+  
+    // We reach here when element is not
+    // present in array
+    return -1;
 }
-
+  
 int main(){
-
-	int arr[] = {1,2,3,4,6,7,9};
-	int n = sizeof(arr)/sizeof(int);
-	int key;
-	cin >> key;
-	binarySearch(arr,n,key);
-	
-	return 0;
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int key = 10;
+    int n = sizeof(arr)/sizeof(int);
+    int result = binarySearch(arr, 0, n-1, key);
+    if(result == -1){
+        cout << "Element is not present in array"<<endl;
+    }
+    else{
+        cout << "Element is present at index " << result;
+    }
+    return 0;
 }

ba9ac659a9c3d91da3e89a854614e67c2db9136f
unknown <bhavishrohilla59@gmail.com>
2021-06-23 14:03:33 +0530

    merge sort recursion

--- recursion/merge_sort.cpp
+++ recursion/merge_sort.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 using namespace std;
 
+void merge(int *a, int s, int e){
+	int mid = (s+e)/2;
+	int i = s;
+	int j = mid + 1;
+	int k = s;
+	int temp[100];
+	while(s<=mid && j<=e){
+		if(a[i]<a[j]){
+			temp[k] = a[i];
+		}
+		else{
+			temp[k] = a[j];
+		}
+	}
+}
+
+void mergeSort(int a[], int s, int e){
+	if(s>=e){
+		return;
+	}
+	//Follow 3 steps
+	//1. Divide
+	int mid= (s+e)/2;
+	//Recusrsively sort arrays -> s to mid and mid+1 to e
+	mergeSort(s,mid);
+	mergeSort(mid+1,e);
+
+	//Merge two parts
+}
+
 int main(){
 	int a[100];
 	int n;

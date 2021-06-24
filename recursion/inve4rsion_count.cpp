ed130f91335293cb881168d66ccc453e57a68ba0
unknown <bhavishrohilla59@gmail.com>
2021-06-24 13:55:29 +0530

    inversion count

--- recursion/inve4rsion_count.cpp
+++ recursion/inve4rsion_count.cpp
@@ -1,11 +1,40 @@
 #include<iostream>
 using namespace std;
 
-void merge(int *a, int s, int e){
+int merge(int *a, int s, int e){
 	int mid = (s+e)/2;
 	int i = s;
 	int j = (mid+1);
 	int k = s;
+	int temp[100000];
+	int cnt = 0;//cross inversion
+	while(i<=mid && j<=e){
+		if(a[i]<=a[j]){
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else{
+			temp[k] = a[j++];
+			cnt += mid-i+1;
+
+		}
+	}
+
+	//fill the arrayif some elements are left in one of the arrays
+	while(i<=mid){
+		temp[k++] = a[i++];
+	}
+	while(j<=e){
+		temp[k++] = a[j++]; 
+	}
+
+	//Copy all elements back to array a
+	for(int i=s; i<=e; i++){
+		a[i] = temp[k];
+	}
+
+	return cnt;
 }
 
 int inversion_count(int *arr, int s, int e){

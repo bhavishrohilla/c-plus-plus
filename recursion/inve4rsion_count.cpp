3fa890a5270b197d844f0a7384c080d3ad957c7f
unknown <bhavishrohilla59@gmail.com>
2021-06-24 11:36:17 +0530

    merge involved

--- recursion/inve4rsion_count.cpp
+++ recursion/inve4rsion_count.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void merge(int *a, int s, int e){
+	int mid = (s+e)/2;
+	int i = s;
+	int j = (mid+1);
+	int k = s;
+}
+
 int inversion_count(int *arr, int s, int e){
 	//base case
 	if(s>=e){
@@ -8,7 +15,10 @@ int inversion_count(int *arr, int s, int e){
 	}
 	//merge sort
 	int mid= (s+e)/2;
-	int x = inversion_count(arr,s,mid);
+	int x = inversion_count(arr	,s,mid);
+	int y = inversion_count(arr, mid+1, e);
+	int z = merge(arr,s,e);//cross inversion
+	return x+y+z;
 }
 
 int main(){

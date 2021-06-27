8f74c1d6608f18b91e646936730737c9e847e417
unknown <bhavishrohilla59@gmail.com>
2021-06-27 11:27:24 +0530

    quick sort recursion

--- recursion/quick_sort.cpp
+++ recursion/quick_sort.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main(){
 
+
+int main(){
+	//Quick sort
 	int arr[] = {2,7,8,6,1,5,4};
-	
+	int n = sizeof(arr)/sizeof(int);
+	quickSort(arr, 0, n-1);
+
+	return 0; 
 }

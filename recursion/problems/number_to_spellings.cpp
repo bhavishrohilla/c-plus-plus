693fe3886f9f3947f51d97cbbc9339c4b7603a44
unknown <bhavishrohilla59@gmail.com>
2021-06-27 16:32:06 +0530

    print spellings

--- recursion/problems/number_to_spellings.cpp
+++ recursion/problems/number_to_spellings.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 char words[][10] = {"Zero","One", "Two", "Three","Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
 void Spell(int n){
 	//base case
-	if(n=0){
+	if(n==0){
 		return;
 	}
 	//rec case
 	Spell(n/10);
-	cout << 
+	int digit = n%10;
+	cout << words[digit] << " ";
 }
 
 int main(){
 	int n;
 	cin >> n;
+	Spell(n);
+
 
 
 

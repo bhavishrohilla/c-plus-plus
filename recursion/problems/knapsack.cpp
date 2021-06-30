be7c6eb2c7ca4a9d098eea4b9fe79fd95905859c
unknown <bhavishrohilla59@gmail.com>
2021-06-30 18:23:34 +0530

    knapsack recursion

---
+++ recursion/problems/knapsack.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int profit(int N, int C, int *wt, int *prices){
+	
+}
+
+int main(){
+
+	int weights[] = {1,2,3,5};
+	int prices[] = {40,20,30,100};
+	int N = 4;
+	int C = 7;
+
+
+	return 0;
+}

7491d922e9816a26ac7e70f568373809fd64a682
unknown <bhavishrohilla59@gmail.com>
2021-06-30 16:45:27 +0530

    generating susequences

---
+++ recursion/problems/tiling_problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void generate_subsequennce(char *in, char *out, int i, int j){
+	//base case
+	if(in[i] == '\0'){
+		out[j] = '\0';
+		cout << out;
+		return;
+		
+	}
+}
+
+int main(){
+	char input[] = "abc";
+	char output[10];
+
+
+	return 0;
+}

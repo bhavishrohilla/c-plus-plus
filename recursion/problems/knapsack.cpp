480abc54c710a221ea8250affa22f15acdf2199d
unknown <bhavishrohilla59@gmail.com>
2021-06-30 18:53:54 +0530

    keypad

--- recursion/problems/knapsack.cpp
+++ recursion/problems/knapsack.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
-#include<cmath>
+#include<climits>
 using namespace std;
 
 int profit(int N, int C, int *wt, int *prices){
 	//base case
-	if(N==0||C=0){
+	if(N==0||C==0){
 		return 0;
 	}
 	//rec
 	int ans = 0;
 	int inc,exc;
-	inc = exc = INT_MIN;
-	if(wt[n-1]<=C){
-		inc = prices[n-1] + profit(N-1,C-wt[n-1],wt,prices);
+	inc = exc = 0;
+	//include
+	if(wt[N-1]<=C){
+		inc = prices[N-1] + profit(N-1,C-wt[N-1],wt,prices);
 	}
+	//exclude
+	exc = profit(N-1,C,wt,prices);
+	ans = max(inc,exc);
+	return ans;
 }
 
 int main(){
@@ -22,7 +27,7 @@ int main(){
 	int prices[] = {40,20,30,100};
 	int N = 4;
 	int C = 7;
-
+	cout << profit(N,C,weights,prices);
 
 	return 0;
 }

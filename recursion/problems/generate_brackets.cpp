88a1ccaff4a4dd63df9fc30ea8f3317536f434a5
unknown <bhavishrohilla59@gmail.com>
2021-06-30 18:00:50 +0530

    generate brackets

---
+++ recursion/problems/generate_brackets.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void generate_brackets(char *out, int n,int idx, int open, int close){
+
+	//base case
+	if(idx == 2*n){
+		cout << out << endl;
+	}
+}
+
+int main(){
+
+	int N;
+	cin >> N;
+	char out[1000];
+	int idx = 0;
+
+	return 0;
+}

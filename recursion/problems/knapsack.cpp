af1ece2c7138185a304631f80306edc19a204b25
unknown <bhavishrohilla59@gmail.com>
2021-06-30 18:29:39 +0530

    excluding the current item

--- recursion/problems/knapsack.cpp
+++ recursion/problems/knapsack.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int profit(int N, int C, int *wt, int *prices){
-	
+	//base case
+	if(N==0||C=0){
+		return 0;
+	}
+	//rec
+	int ans = 0;
+	int inc,exc;
+	inc = exc = INT_MIN;
+	if(wt[n-1]<=C){
+		inc = prices[n-1] + profit(N-1,C-wt[n-1],wt,prices);
+	}
 }
 
 int main(){

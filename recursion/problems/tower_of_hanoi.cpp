0beef34d2b9ee97100fc52a85995128b63aa0b62
unknown <bhavishrohilla59@gmail.com>
2021-06-30 16:04:53 +0530

    tower problem

--- recursion/problems/tower_of_hanoi.cpp
+++ recursion/problems/tower_of_hanoi.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void move(int n, char src, char dest, char helper){
-	
+void move(int n, char src, char helper, char dest){
+
 }
 
 int main(){
 
 	int n;
 	cin >> n;
-	move(n, 'A', 'C');
+	move(n, 'A', 'B', 'C');
 
 	return 0;
 }

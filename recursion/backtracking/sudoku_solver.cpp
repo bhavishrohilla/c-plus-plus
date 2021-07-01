66c514af9f9414bd0601805242568ce4a7e84c76
unknown <bhavishrohilla59@gmail.com>
2021-07-01 19:01:44 +0530

    subgrid check and done

--- recursion/backtracking/sudoku_solver.cpp
+++ recursion/backtracking/sudoku_solver.cpp
@@ -1,10 +1,37 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+	
+bool canPlace(int mat[][9], int i, int j, int n, int number){
+	for(int x=0; x<n; x++){
+		//Row and colmn check
+		if(mat[x][j]==number||mat[i][x]==number){
+			return false;
+		}
+	}
+	int rn = sqrt(n);
+	int sx = (i/rn)*rn;
+	int sy = (j/rn)*rn;
+	for(int x=sx; x<sx+rn; x++){
+		for(int y=sy; y<sy+rn; y++){
+			if(mat[x][y]==number){
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
 bool solveSudoku(int mat[][9], int i, int j, int n){
 	//base case
 	if(i==n){
 		//Print the matrix
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				cout << mat[i][j]<<" ";
+			}
+			cout << endl;
+		}
 		return true;
 	}
 	//Case row end
@@ -20,9 +47,18 @@ bool solveSudoku(int mat[][9], int i, int j, int n){
 	//Fill the current cell with possible options
 	for(int number=0; number<=n; number++){
 		if(canPlace(mat,i,j,n,number)){
-			
+			//Assume
+			mat[i][j]==number;
+
+			bool couldWeSolve = solveSudoku(mat,i,j+1,n);
+			if(couldWeSolve==true){
+				return true;
+			}
 		}
 	}
+	//Backtrack
+	mat[i][j] =0;
+	return false;
 }
 
 int main(){
@@ -37,7 +73,7 @@ int main(){
 		{0,0,0,4,1,9,0,0,5},
 		{0,0,0,0,8,0,0,7,9},
 	};
-
+	cout << solveSudoku(mat,0,0,9);
 
 	return 0;
 }

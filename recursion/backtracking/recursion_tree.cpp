2bc641f3821890d699c7f78f1b21de6ab5d4f0a8
unknown <bhavishrohilla59@gmail.com>
2021-07-01 18:16:20 +0530

    recursion tree backtrack

--- recursion/backtracking/recursion_tree.cpp
+++ recursion/backtracking/recursion_tree.cpp
@@ -4,10 +4,15 @@ using namespace std;
 void permute(char*in, int i){
 	//base base
 	if(in[i]=='\0'){
-		cout<<in<<endl;
+		cout<<in<<",";
 		return;
 	}
 	//rec case
+	for(int j=i; in[j]!='\0'; j++){
+		swap(in[i],in[j]);
+		permute(in,i+1);
+		swap(in[i],in[j]);	//Backtracking -> restoring the original array
+	}
 }
 
 int main(){

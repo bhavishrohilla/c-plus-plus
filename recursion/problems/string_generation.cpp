7cce8dc9343ad653ba5aea44f073076a40ce3706
unknown <bhavishrohilla59@gmail.com>
2021-07-01 14:17:41 +0530

    2 digits at a time

--- recursion/problems/string_generation.cpp
+++ recursion/problems/string_generation.cpp
@@ -17,6 +17,15 @@ void generate_strings(char *in, char *out, int i, int j){
 	generate_strings(in,out,i+1,j+1);
 
 	//two digit at a time
+	if(in[i+1]!='\0'){
+		int secondDigit = in[i+1] - '0';
+		int no = digit*10 + secondDigit;
+		if(no<=26){
+			ch = no + 'A' - 1;
+			out[j] = ch;
+			generate_strings(in,out,i+2,j+1);
+		}
+	}
 }
 
 int main(){

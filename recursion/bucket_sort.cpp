c6c07217fc39c3fa9a78c372df95097e3bc79ba0
unknown <bhavishrohilla59@gmail.com>
2021-07-02 17:03:39 +0530

    bucket sort

---
+++ recursion/bucket_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Student{
+	int marks;
+	string name;
+};
+
+//Buckety sort to sort an array of students
+
+void bucket_sort(Student s[], int n){
+
+	//Assuming marks are in range 0-100
+	vector<Student> v[101];
+	
+	//O(N) time
+	for(int i=0; i<n;i++){
+		int m = s[i].marks;
+		v[m].push_back(s[i]);
+	}
+
+	//Iterate over the vector and print the students
+	for(*)
+}
+
+int main(){
+	Student s[10000];
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		cin>>s[i].marks >> s[i].name;
+	}
+
+
+	return 0;
+}

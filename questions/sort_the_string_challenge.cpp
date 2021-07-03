f451ef72307a51a0f4e03a6365d310f5a6132e53
unknown <bhavishrohilla59@gmail.com>
2021-07-03 13:44:55 +0530

    sorting the string

---
+++ questions/sort_the_string_challenge.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin.get();
+	string a[100];
+	for(int i=0; i<n; i++){
+		getline(cin,a[i]);
+	}
+
+	int key;
+	string reversal,ordering;
+
+
+	return 0;
+}

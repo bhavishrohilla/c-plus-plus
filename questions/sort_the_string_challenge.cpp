489449260e9e957dcc6e20cbf732974b1262725c
unknown <bhavishrohilla59@gmail.com>
2021-07-03 13:59:21 +0530

    sorting

--- questions/sort_the_string_challenge.cpp
+++ questions/sort_the_string_challenge.cpp
@@ -14,11 +14,6 @@ string extractStringAtKey(string str, int key){
 
 int main(){
 
-	string s("10 20 30 40 50");
-	int key;
-	cin >> key;
-	cout << extractStringAtKey(s, key);
-	/*
 	int n;
 	cin.get();
 	string a[100];
@@ -28,7 +23,18 @@ int main(){
 
 	int key;
 	string reversal,ordering;
-	*/
+	pair<string,string> strPair[100];
+	for(int i=0; i<n; i++){
+		strPair[i].first = a[i];
+		strPair[i].second = extractStringAtKey(a[i],key);
+	}
+	//Need to sort
+	if(ordering == "numeric"){
+		sort(strPair,strPair+n,numericCompare);
+	}
+	else{
+		sort(strPair, strPair+n, lexicoCompare);
+	}
 
 	return 0;
 }

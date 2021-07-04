3b64425b1c0bb9d7d2ac6ea01ccd120f4d65efbc
unknown <bhavishrohilla59@gmail.com>
2021-07-04 20:38:22 +0530

    shallow deep drawback

--- OOPs/car_shallow_deep.cpp
+++ OOPs/car_shallow_deep.cpp
@@ -1,4 +1,5 @@
-#include<iosream>
+#include<iostream>
+#include<cstring>
 using namespace std;
 
 class Car{
@@ -14,14 +15,27 @@ public:
 	}
 	Car(float p, int m, char *n){
 		price = p;
-		model = m;
-		
+		model_no = m;
+		name = new char[strlen(n)+1];
+		strcpy(name,n);
+	}
+	void set_Price(int p){
+		price = p;
+	}
+
+	void print(){
+		cout << "Name " << name<<endl;
+		cout << "Model No "<< model_no << endl;
+		cout << "Price "<<price<<endl;
 	}
 };
 
 int main(){
-
-
+	Car c(111,100,"BMW");	//parametrised constructor
+	Car d(c);	//Copy COnstructor
+	c.name[0] ='A';
+	c.print();
+	d.print();
 
 	return 0;
 }

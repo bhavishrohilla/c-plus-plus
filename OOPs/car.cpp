4430c2ac262d1c21ab8ae8f930ea90e3ca282bc1
unknown <bhavishrohilla59@gmail.com>
2021-07-04 16:49:01 +0530

    data members in class

--- OOPs/car.cpp
+++ OOPs/car.cpp
@@ -14,9 +14,16 @@ int main(){
 	Car arr[20];	//store info of max 20 cars
 	Car c;
 
-	cout <<"C-Name " <<sizeof(c.name) << endl;
-	cout <<"C-Price " <<sizeof(c.price) << endl;
-	cout <<"C-model " <<sizeof(c.model_no) << endl;
+	c.price = 100;
+	c.model_no = 112;
+	c.name[0] = 'B';
+	c.name[1] = 'M';
+	c.name[2] = 'W';
+	c.name[3] = '\0';
+
+	cout <<"C-Name " <<sizeof(c.name) << " "<<c.name <<endl;
+	cout <<"C-Price " <<sizeof(c.price) <<" "<<c.price <<endl;
+	cout <<"C-model " <<sizeof(c.model_no)<<" "<<c.model_no << endl;
 	cout << "Car " << sizeof(Car) << endl;
 
 

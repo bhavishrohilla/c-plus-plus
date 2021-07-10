097de2f0fd069c41abfe0eb8ba899b7057db6b09
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-10 10:57:07 +0530

    iterators

--- GenericProgramming/templates.cpp
+++ GenericProgramming/templates.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 template<typename T>
@@ -12,7 +13,19 @@ int search(T arr[], int n, T key){
 	return n;
 }
 
+template<class ForwardIterator, class T>
+ForwardIterator search(ForwardIterator start, ForwardIterator end, T key){
+	while(start!=end){
+		if(*start==key){
+			return start;
+		}
+		start++;
+	}
+	return end;
+}
+
 int main(){
+	/*
 	int a[] = {1,5,3,7,8,6,2};
 	int n = sizeof(a)/sizeof(int);
 	int key = 7;
@@ -21,5 +34,21 @@ int main(){
 	float k = 1.3;
 	//cout << search(a,n,key);
 	cout << search(b,3,k);
+	*/
+	list<int>l;
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(5);
+	l.push_back(3);
+	auto it = search(l.begin(),l.end(),5);
+	if(it==l.end()){
+		cout << "Element not present";
+	}
+	else{
+		cout << *it << endl;
+	}
+
+
+
 	return 0;
 }

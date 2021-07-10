883366c77f9b2ffb0a4a69dcceed486ecf73ab32
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-10 11:36:45 +0530

    3 ways to iterate over vectors

--- vectors/vector_demo.cpp
+++ vectors/vector_demo.cpp
@@ -11,9 +11,24 @@ int main(){
 
 	vector<int> d{1,2,3,10,13};
 
-	//Iterate over thr vector
+	//Iterate over the vector
+
+	//1st way
 	for(int i=0; i<c.size();i++){
 		cout << c[i]<<",";
+	}
+	cout << endl;
+
+	//2nd Way
+	for(auto it=b.begin();it!=b.end();it++){	//vector<int>::iterator is the class here for 'auto'
+		cout << *it<<",";
+	}
+	cout << endl;
+
+	//3rd Way
+	//for each loop
+	for(int x:d){
+		cout << x<<",";
 	}
 
 

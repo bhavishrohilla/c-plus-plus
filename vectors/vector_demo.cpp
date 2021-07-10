71574f5a77134dfa3ba753dc01ded2e4a704ec52
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-10 11:29:15 +0530

    create and initialize a vectors

--- vectors/vector_demo.cpp
+++ vectors/vector_demo.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(){
+
+	//Create and initilize a vector
 	vector<int> v;
+	vector<int> b(5,10);//five integers in an array and they have a value 10
+	vector<int> c(b.begin(),b.end());
+
+	vector<int> d{1,2,3,10,13};
 
 	return 0;
 }

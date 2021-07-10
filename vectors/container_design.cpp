4669bb37ff5aaf5402847c8f41331ff6d0ebd522
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-10 22:40:16 +0530

    user defined vector class

--- vectors/container_design.cpp
+++ vectors/container_design.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main(){
 	Vector v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
+	v.push_back(1);	//Capacity 1
+	v.push_back(2);	//Capacity 2
+	v.push_back(3);	//Capacity 4(push_back will add element as well double the size of the array)
 	v.push_back(4);
 
 	v.pop_back();
-	v.push_back(6);
+	v.push_back(6); //Capacity 4 (as 4 elements can accomodate in the array of size 4)
+	v.push_back(7); //Capacity 8 
+
+	cout <<"Capacity "<< v.capacity()<<endl;
 
 	for(int i=0; i<v.size();i++){
 		cout << v[i] <<" ";

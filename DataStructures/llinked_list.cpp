4227ba041987650a3bfeca4041bbe9fef6598451
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-11 14:53:36 +0530

    linked list delete head

--- DataStructures/llinked_list.cpp
+++ DataStructures/llinked_list.cpp
@@ -54,15 +54,74 @@ void print(node*head){
 	cout << endl;
 }
 
+int length(node*head){
+	int cnt = 0;
+	while(head!=NULL){
+		cnt++;
+		head = head->next;
+	}
+	return cnt;
+}
+
+void insertAtTail(node*&head, int data){
+	if(head==NULL){
+		head = new node(data);
+		return;
+	}
+	node*tail = head;
+	while(tail->next!=NULL){
+		tail = tail->next;
+	}
+	tail->next = new node(data);
+	return;
+}
+
+void insertInMiddle(node*&head, int data, int p){
+	 if(head==NULL or p==0){
+	 	insertAtHead(head,data);
+	 }
+	 else if(p>length(head)){
+	 	insertAtTail(head,data);
+
+	 }
+	 else{
+	 	//take p-1 jumps 
+	 	int jump = 1;
+	 	node*temp = head;
+	 	while(jump<=p-1){
+	 		temp = temp->next;
+	 		jump++;
+	 	}
+
+	 	//create a new node
+	 	node*n = new node(data);
+	 	n->next = temp-> next;
+	 	temp->next = n;
+	 }
+}
+
+void deleteHead(node*head){
+	if(head==NULL){
+		return;
+	}
+	node*temp = head->next;
+	delete head;
+	head = temp;
+}
+
 int main(){
 
 	node*head = NULL;
-	insertAtHead(head,3);
+	insertAtHead(head,5);
 	insertAtHead(head,2);
 	insertAtHead(head,1);
 	insertAtHead(head,0);
-
 	print(head);
+
+	insertInMiddle(head,4,3);
+	insertAtTail(head,7);
+	print(head);
+	deleteHead(head);
 	print(head);
 }
 	

79a9fb8f2fbe0a69eaf49a16c74e341bd3d937b9
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-14 19:18:48 +0530

    methods in list class in STL

--- DataStructures/list_STL.cpp
+++ DataStructures/list_STL.cpp
@@ -12,10 +12,6 @@ int main(){
 	list<string> l2{"apple","mango","banana"};
 	l2.push_back("pineapple");
 
-	//Iterartion
-	for(auto s:l2){
-		cout << s << "->";
-	}
 
 	l2.sort();
 
@@ -27,5 +23,35 @@ int main(){
 
 	l2.push_front("kiwi");
 	l2.pop_back();
+	//Iterartion
+	for(auto s:l2){
+		cout << s << "->";
+	}
+	l2.push_back("Carrot");
+	l2.push_back("Lemon");
+
+	//remove a fruit
+	string f;
+	cin >> f;
+	l2.remove(f);	//All the occurence of f will be removed
+
+
+	//erase one or more
+	auto it = l2.begin();
+	it++;
+	it++;
+
+	//insertelements in the list
+	it = l2.begin();
+	it++;
+	l2.insert(it, "fruit juice");
+
+
+
+
+
+	l2.erase(it);
+
+
 
 }

a93be184a681798f5d6236ef258cd5aba3197581
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-14 18:55:04 +0530

    deleting a node in circular linked list

--- DataStructures/circular_Li.cpp
+++ DataStructures/circular_Li.cpp
@@ -13,3 +13,56 @@ public:
 
 	}
 };
+
+void insert(node*head, int data){
+	node* n = new node(data);
+	node*temp = head;
+
+	n->next = head;
+	if(temp!=NULL){
+		while(temp->next!=head){
+			temp = temp->next;
+		}
+
+		temp->next = n;
+	}
+	head = n;
+}
+void print(node*head){
+	node*temp = head;
+	while(temp->next!=head){
+		cout << temp->data<<" ";
+		temp = temp->next;
+	}
+	cout << temp->data;
+	return;
+}
+
+//delete node in circular ll
+node*getnode(node*&head, int data){
+	node*temp = head;
+	while(temp->next!=head){
+		if(temp->data==data){
+			return temp;
+		}
+		temp = temp->next;
+	}
+
+	//out of the loop
+	if(temp->data=data){
+		return temp;
+	}
+	return NULL;
+}
+ 
+int main(){
+
+	node*head = NULL;
+	insert(head,10);
+	insert(head,20);
+	insert(head,30);
+	insert(head,40);
+	insert(head,50);
+	insert(head,60);
+	return 0;
+}

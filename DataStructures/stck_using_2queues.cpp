b039e3220e4c92dd012ec92853c5daeb47383a30
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-20 08:14:54 +0530

    stack using queue

--- DataStructures/stck_using_2queues.cpp
+++ DataStructures/stck_using_2queues.cpp
@@ -15,17 +15,49 @@ public:
 			return;
 		}
 		while(q1.size()>1){
-			
+			T element  = q1.front();
+			q2.push(element);
+			q1.pop();
+
 		}
+		q1.pop();
+
+		//swap the names of q1 q2
+		swap(q1,q2);
 	}
 	int top(){
+		while(q1.size()>1){
+			T element  = q1.front();
+			q2.push(element);
+			q1.pop();
+		}
 
+		//1 element in q1
+		T element = q1.front();
+		q1.pop();
+		q2.push(element);
+
+		swap(q1,q2);
+	}
+	int size(){
+		return q1.size() + q2.size();
+	}
+	bool empty(){
+		return size()==0;
 	}
 };
 
 int main(){
 
+	Stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
 
+	while(!s.empty()){
+		cout << s.top()<<" ";
+		s.pop();
+	}
 
 
 	return 0;

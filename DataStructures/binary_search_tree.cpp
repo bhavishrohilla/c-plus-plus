bd3aae6783157aaf59ccdfadf34bbed1137229f2
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-07-20 10:57:23 +0530

    binary search tree- Inorder and Postorder

--- DataStructures/binary_search_tree.cpp
+++ DataStructures/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<deque>
 using namespace std;
 
 class node{
@@ -17,14 +18,54 @@ public:
 node* buildTree(){
 	int d;
 	cin >> d;
+
 	if(d==-1){
 		return NULL;
 	}
+
+	node * root = new node(d);
+	root->left = buildTree();
+	root->right = buildTree();
+	return root;
 }
 
+void print(node *root){
+	cout << root->data<<" ";
+	print(root->left);
+	print(root->right);
+}
+
+void printIn(node*root){
+	if(root==NULL){
+		return;
+	}
+	//Oterwise Left Root Right
+	printIn(root->left);
+	cout << root->data<<" ";
+	printIn(root->right);
+}
+void printPost(node*root){
+	if(root==NULL){
+		return;
+	}
+	//Oterwise Left Root Right
+	printPost(root->left);
+	printPost(root->right);
+	cout << root->data<< " ";
+}
 
 int main(){
+	node *root = buildTree();
+	print(root);
+	cout << endl;
 
+	printIn(root);
+	printPost(root);
 
 	return 0;
 }
+
+
+/*
+	/s/19425
+*/
